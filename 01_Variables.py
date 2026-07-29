a58ce68ae922f2347517f0448824619ea772d0b7
Agente-707 <lupareja2025@gmail.com>
2026-07-29 11:07:23 -0500

    variables de Python

---
+++ 01_Variables.py
@@ -0,0 +1,32 @@
+#Variables
+
+#string
+my_string = "My String Variable"
+print(my_string)
+
+#int
+my_int = 10
+print(my_int)
+
+#float
+my_float = 5.3
+print(my_float)
+
+#boolean
+my_boolean = True
+print(my_boolean)
+
+#type
+print(type(my_string))
+print(type(my_int))
+print(type(my_float))
+print(type(my_boolean))
+
+#tranformacion de variables a cadena de texto
+print(my_string, my_int, my_float, my_boolean);
+
+#str 
+my_int_to_string = str(my_int)
+print(type(my_int_to_string))
+my_float_to_string = str(my_float)
+print(type(my_float_to_string))
