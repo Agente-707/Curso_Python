8ba3eb3f9659fdd9f65522a1557410ca145e068e
Agente-707 <lupareja2025@gmail.com>
2026-07-29 10:40:01 -0500

    ejercicio Hola Mundo

---
+++ 00_Hello_World.py
@@ -0,0 +1,19 @@
+# Este es un comentario
+
+"""
+Este es un
+comentario de
+varias lineas
+"""
+
+'''
+Este tambien es un
+comentario de
+varias lineas con
+comillas simples
+'''
+
+# Hola Mundo en Python
+print("Hola Mundo") #comilla doble
+
+print('Hola Mundo') #comilla simple
