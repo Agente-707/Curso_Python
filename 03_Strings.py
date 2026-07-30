1bb0548e01881d6bc4f71f0e7b7d2119428f0674
Agente-707 <lupareja2025@gmail.com>
2026-07-30 18:45:14 -0500

    clase de strings

---
+++ 03_Strings.py
@@ -0,0 +1,16 @@
+### Strings ###
+
+my_string = "Mi String"
+my_string2 = 'mi otro string'
+
+print(my_string + " " + my_string2) # Concatenación
+
+my_string3 = "Este es un string\ncon salto de linea" #salto de linea
+print(my_string3)
+
+my_string4 = "Este es un string\tcon tabulacion" #tabulacion
+print(my_string4)
+
+my_string5 = "\\tEste es un String \\n escapado" #caracteres escapados
+print(my_string5)
+
