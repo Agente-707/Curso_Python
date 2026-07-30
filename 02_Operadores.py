260df106f59c6063747019e8ec94ed6ff72bea7f
Agente-707 <lupareja2025@gmail.com>
2026-07-30 10:06:46 -0500

    Operaciones aritmeticas, comparativas y logicas

---
+++ 02_Operadores.py
@@ -0,0 +1,48 @@
+### Operadores Aritmeticos ###
+
+print(3+4) # Suma
+print(3-4) # Resta
+print(3*4) # Multiplicación
+print(3/4) # División
+print(3%4) # Módulo
+print(10//3) # División entera
+print(2**3) # Potencia
+
+# Concatenación de cadenas
+print("Hola" + " Mundo") 
+print("Tengo " + str(19) + " años")
+
+# Repetición de cadenas
+print("Hola " * 5) 
+print("Hola " * (2**3))
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float)) 
+
+### Operadores Comparativos ###
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+print("Hola" > "Mundo")
+print("Hola" < "Mundo")
+print("Hola" >= "Mundo")
+print("Hola" <= "Mundo")
+print("Hola" == "Mundo")
+print("Hola" != "Mundo")
+
+
+print("aaaa" >= "abaa") # Operaciones alfabeticas
+print(len("aaaa") >= len("abaa")) # Cuenta caracteres y compara
+
+### Operadores Logicos ###
+print(3 > 4 and "Hola" > "Mundo")
+print(3 > 4 or "Hola" > "Mundo")
+print(3 < 4 and "Hola" < "Mundo")
+print(3 < 4 or "Hola" > "Mundo")
+print(3 < 4 or ("Hola" > "Mundo" and 4 == 4))
+print(not(3>4))
