b84655f9a1d9026c0cecb0c2dda8d7177e8be580
Agente-707 <lupareja2025@gmail.com>
2026-08-07 12:44:23 -0500

    Bucle for

--- 09_Bucles.py
+++ 09_Bucles.py
@@ -30,9 +30,10 @@ print("La ejecución continúa")
 # Condicional if dentro de while
 while my_condition < 20:
     my_condition += 1 # my_condition ira aumentando de uno en uno
-    if my_condition === 15: # Cuando my_condition sea 15 imprimira el mensaje 
+    if my_condition == 15: # Cuando my_condition sea 15 imprimira el mensaje 
         print("Mi condición es 15")
-    print(my_condition)
+    else: # si no imprime solo el número
+        print(my_condition)
 
 # break
 my_condition = 10
@@ -47,9 +48,36 @@ while my_condition < 20:
 # for
 my_list = [12, 23, 11, 46, 18]
 
-for element in my_list:
+for element in my_list: # el for va a repetir tantas veces como elementos tengamos en nuestra lista
+    print(element) # en cada vuelta va a acceder a cada valor de la lista en orden
+
+# el for funciona con cualquier tipo de estructura de elementos
+
+# tupla
+my_tuple = (19, 1.75, "Luis", "Pareja")
+for element in my_tuple: 
     print(element)
 
+# set
+my_set = {"Luis", "Pareja", 19}
+for element in my_set: 
+    print(element)
 
+# diccionario
+my_dict = {"Name":"Luis", "Surname":"Pareja", "Age":19, 10:"hola"} 
+for element in my_dict: 
+    print(element) # imprime las llaves no los valores
 
+# else en el bucle for
+for element in my_list:
+    print(element)
+else: # cuando el bucle for termine, imprimira el mensje
+    print("mi bulce for a terminado")
 
+for element in my_list:
+    print(element)
+    if element == 11:
+        break  # El break tambien corta con el bucle for
+    print("Se ejecuta")
+else:
+    print("El bucle for a terminado")    
