0b9dfdd793373e86b3db5c0788f1fcccb6cd6749
Agente-707 <lupareja2025@gmail.com>
2026-08-13 11:04:11 -0500

    Excepciones en Python

--- 12_Excepciones.py
+++ 12_Excepciones.py
@@ -5,10 +5,44 @@ numberTwo = 5
 
 print(numberTwo + numberOne) # Operación válida
 
-# Pasamos 'numberTwo' de 'int' a 'str'
-# numberTwo = "1"
+#Pasamos 'numberTwo' de 'int' a 'str'
+numberTwo = "1"
 # print(numberTwo + numberOne) => Error: No se puede sumar un entero con un string
 
+'''
+Nota:
+Podemos controlar este error
+usando condicionales
+'''
+
+# Controlando error con condicionales
+if type(numberTwo) == int: # type(numberTwo) = str => No se cumple
+    print(numberOne + numberTwo) # No se ejecuta el error
+else:
+    print("No se cumplió") 
+# => "No se cumplió"
 
 
+'''
+Nota:
+La mejor práctica para 
+controlar errores es usando
+'try' y 'except'
+'''
 
+# Controlando errores con 'try except'
+try:
+    print(numberOne + numberTwo) # Si se produce un error se salta al bloque de 'except'
+    print("No se ha producido un error") # Si no hay error el mensaje se ejecuta y se salta el bloque de except
+except:
+    print("Se ha producido un error")
+# => Se ha producido un error
+
+'''
+Nota: 
+En este caso, el error se ejecuta pero
+nuestro programa no se cierra
+'''
+
+
+# try except else
