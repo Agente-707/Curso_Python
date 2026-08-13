3def70d5a8fbb4bfcd61df4b83a0888c90fd870c
Luis Augusto <lupareja2505@gmail.com>
2026-08-13 10:47:11 -0500

    Enhance exception handling examples in 12_Excepciones.py
    Added example of valid operation and commented error case.

--- 12_Excepciones.py
+++ 12_Excepciones.py
@@ -1,1 +1,14 @@
 ### Gestión de excepciones ###
+
+numberOne = 2
+numberTwo = 5
+
+print(numberTwo + numberOne) # Operación válida
+
+# Pasamos 'numberTwo' de 'int' a 'str'
+# numberTwo = "1"
+# print(numberTwo + numberOne) => Error: No se puede sumar un entero con un string
+
+
+
+
