ed6635648f7b83136994ec5337a7da15aec86986
Luis Augusto <lupareja2505@gmail.com>
2026-08-14 09:51:22 -0500

    Add sumValue and printValue functions
    Added functions for summing values and printing.

--- my_module.py
+++ my_module.py
@@ -1,6 +1,14 @@
+'''
+Nota:
+Desde este archivo vamos a llamar
+a las funciones 'SumValue' y 'printValue'
+y aplicarlas dentro del archivo '12_Modulos.py'
+'''
 
+# Función de suma
 def sumValue(numberOne, numberTwo, numberThree):
     print(numberOne + numberTwo + numberThree)
 
+# Función de imprimir un valor
 def printValue(value):
     print(value)
